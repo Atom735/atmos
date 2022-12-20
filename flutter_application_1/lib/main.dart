92a8d8f6b2b79c432b6c717d2eabf8945a6f7b19
Gilyazeev Adel <Atom735.ag47@gmail.com>
2022-12-20 21:26:10 +0300

    List of chat

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -13,15 +15,20 @@ class MyApp extends StatelessWidget {
   Route? onGenerateRoute(RouteSettings settings) {
     final name = settings.name ?? '';
     switch (name) {
+      case '/':
+        return MaterialPageRoute(
+          settings: settings,
+          builder: (context) => const MyInitialScreen(),
+        );
       case '/auth':
         return MaterialPageRoute(
           settings: settings,
           builder: (context) => const MyAuthScreen(),
         );
-      case '/':
+      case '/chats':
         return MaterialPageRoute(
           settings: settings,
-          builder: (context) => const MyInitialScreen(),
+          builder: (context) => const MyChatsScreen(),
         );
       default:
     }
@@ -45,16 +52,27 @@ class MyAuthScreen extends StatefulWidget {
   State<MyAuthScreen> createState() => _MyAuthScreenState();
 }
 
+Widget _showLoadingDialogBuilder(BuildContext context) {
+  return WillPopScope(
+    child: const Center(child: CircularProgressIndicator.adaptive()),
+    onWillPop: () => SynchronousFuture(false),
+  );
+}
+
+Future<void> showLoadingDialog(BuildContext context) {
+  return showDialog(
+    context: context,
+    builder: _showLoadingDialogBuilder,
+    barrierDismissible: false,
+  );
+}
+
 class _MyAuthScreenState extends State<MyAuthScreen> {
   final _login = TextEditingController();
   final _password = TextEditingController();
 
   Future<void> submit() async {
-    showDialog(
-      context: context,
-      builder: buildLoading,
-      barrierDismissible: false,
-    );
+    showLoadingDialog(context);
     var complete = false;
     try {
       final token = await StudyJamClient().signin(_login.text, _password.text);
@@ -68,13 +86,6 @@ class _MyAuthScreenState extends State<MyAuthScreen> {
       if (!mounted) return;
       Navigator.of(context).restorablePushReplacementNamed('/');
     }
-  }
-
-  Widget buildLoading(BuildContext context) {
-    return WillPopScope(
-      child: const Center(child: CircularProgressIndicator.adaptive()),
-      onWillPop: () => SynchronousFuture(false),
-    );
   }
 
   @override
@@ -181,16 +192,109 @@ class _MyInitialScreenState extends State<MyInitialScreen> {
   }
 }
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+class MyChatsScreen extends StatefulWidget {
+  const MyChatsScreen({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<MyChatsScreen> createState() => _MyChatsScreenState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _MyChatsScreenState extends State<MyChatsScreen> {
+  var lastUpdateChats = DateTime(2022);
+
+  late final client = SharedAppData.getValue<Type, StudyJamClient>(
+    context,
+    StudyJamClient,
+    () => StudyJamClient(),
+  );
+
+  final chats = <int, SjChatDto>{};
+
+  var initial = true;
+  @override
+  void didChangeDependencies() {
+    if (initial) {
+      initial = false;
+      loadData();
+    }
+    super.didChangeDependencies();
+  }
+
+  Future<void> loadData() async {
+    await Future.delayed(const Duration(microseconds: 1));
+    if (!mounted) return;
+    showLoadingDialog(context);
+    try {
+      final sp = await SharedPreferences.getInstance();
+      lastUpdateChats = DateTime.parse(
+        sp.getString('lastUpdateChats') ?? DateTime(2022).toIso8601String(),
+      );
+      final chats = jsonDecode(sp.getString('chats') ?? '{}') as Map?;
+      if (chats != null) {
+        this.chats.addAll(chats.map((key, value) =>
+            MapEntry(int.parse(key), SjChatDto.fromJson(value))));
+      }
+    } finally {
+      Navigator.pop(context);
+    }
+    await refresh();
+  }
+
+  Future<void> refresh() async {
+    showLoadingDialog(context);
+    try {
+      final sp = await SharedPreferences.getInstance();
+
+      final indexes = await client.getUpdates(
+        chats: lastUpdateChats,
+      );
+
+      final chatsI = indexes.chats;
+      if (chatsI?.isNotEmpty ?? false) {
+        chatsI!;
+        final l = ((chatsI.length - 1) ~/ 1000) + 1;
+        for (var i = 0; i < l; i++) {
+          final chats = await client.getChatsByIds(
+            chatsI.skip(i * 1000).take(1000).toList(),
+          );
+          for (final e in chats) {
+            if (e.updated.isAfter(lastUpdateChats)) {
+              lastUpdateChats = e.updated;
+            }
+            this.chats[e.id] = e;
+          }
+        }
+        setState(() {});
+      }
+
+      await sp.setString('lastUpdateChats', lastUpdateChats.toIso8601String());
+      final chats = this.chats.map(
+            (key, value) => MapEntry(key.toString(), value.toJson()),
+          );
+      await sp.setString('chats', jsonEncode(chats));
+    } finally {
+      Navigator.pop(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ListView(
+        children: chats.values
+            .map(
+              (e) => ListTile(
+                leading: e.avatar == null || e.avatar!.isEmpty
+                    ? CircleAvatar(
+                        child: Text('${e.id}'),
+                      )
+                    : Image.network(e.avatar!),
+                title: Text('#${e.id}: ${e.name}'),
+                subtitle: e.description == null ? null : Text(e.description!),
+              ),
+            )
+            .toList(),
+      ),
+    );
   }
 }
