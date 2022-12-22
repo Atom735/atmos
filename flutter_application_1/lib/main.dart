62280b23599790cb54dcaf982ba3e1260d8992bf
Gilyazeev Adel <Atom735.ag47@gmail.com>
2022-12-22 15:58:29 +0300

    Getted chats and msgs

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -14,23 +15,31 @@ class MyApp extends StatelessWidget {
 
   Route? onGenerateRoute(RouteSettings settings) {
     final name = settings.name ?? '';
+    if (name.startsWith(MyChat.routeNamePrefix)) {
+      return MaterialPageRoute(
+        settings: settings,
+        builder: MyBuilderWrapper(MyChat(MyChat.routeNameChatId(name))).build,
+      );
+    }
     switch (name) {
-      case '/':
+      case MyInitialScreen.routeName:
         return MaterialPageRoute(
           settings: settings,
-          builder: (context) => const MyInitialScreen(),
+          builder: const MyBuilderWrapper(MyInitialScreen()).build,
         );
-      case '/auth':
+      case MyAuthScreen.routeName:
         return MaterialPageRoute(
           settings: settings,
-          builder: (context) => const MyAuthScreen(),
+          builder: const MyBuilderWrapper(MyAuthScreen()).build,
         );
-      case '/chats':
+      case MyChatsScreen.routeName:
         return MaterialPageRoute(
           settings: settings,
-          builder: (context) => const MyChatsScreen(),
+          builder: const MyBuilderWrapper(MyChatsScreen()).build,
         );
       default:
+        debugger();
+        return null;
     }
   }
 
@@ -45,8 +54,21 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class MyBuilderWrapper extends StatelessWidget {
+  const MyBuilderWrapper(this.child, {super.key});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return child;
+  }
+}
+
 class MyAuthScreen extends StatefulWidget {
   const MyAuthScreen({super.key});
+
+  static const routeName = '/auth';
 
   @override
   State<MyAuthScreen> createState() => _MyAuthScreenState();
@@ -84,7 +106,8 @@ class _MyAuthScreenState extends State<MyAuthScreen> {
     }
     if (complete) {
       if (!mounted) return;
-      Navigator.of(context).restorablePushReplacementNamed('/');
+      Navigator.of(context)
+          .restorablePushReplacementNamed(MyInitialScreen.routeName);
     }
   }
 
@@ -144,6 +167,8 @@ class _MyAuthScreenState extends State<MyAuthScreen> {
 class MyInitialScreen extends StatefulWidget {
   const MyInitialScreen({super.key});
 
+  static const routeName = Navigator.defaultRouteName;
+
   @override
   State<MyInitialScreen> createState() => _MyInitialScreenState();
 }
@@ -171,14 +196,17 @@ class _MyInitialScreenState extends State<MyInitialScreen> {
         if (!mounted) return;
         SharedAppData.setValue(context, SjUserDto, user);
         SharedAppData.setValue(context, StudyJamClient, client);
-        Navigator.of(context).restorablePushReplacementNamed('/chats');
+        Navigator.of(context)
+            .restorablePushReplacementNamed(MyChatsScreen.routeName);
       } catch (e) {
         if (!mounted) return;
-        Navigator.of(context).restorablePushReplacementNamed('/auth');
+        Navigator.of(context)
+            .restorablePushReplacementNamed(MyAuthScreen.routeName);
       }
     } else {
       if (!mounted) return;
-      Navigator.of(context).restorablePushReplacementNamed('/auth');
+      Navigator.of(context)
+          .restorablePushReplacementNamed(MyAuthScreen.routeName);
     }
   }
 
@@ -194,6 +222,8 @@ class _MyInitialScreenState extends State<MyInitialScreen> {
 
 class MyChatsScreen extends StatefulWidget {
   const MyChatsScreen({super.key});
+
+  static const routeName = '/chats';
 
   @override
   State<MyChatsScreen> createState() => _MyChatsScreenState();
@@ -280,21 +310,178 @@ class _MyChatsScreenState extends State<MyChatsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: chats.values
-            .map(
-              (e) => ListTile(
-                leading: e.avatar == null || e.avatar!.isEmpty
-                    ? CircleAvatar(
-                        child: Text('${e.id}'),
-                      )
-                    : Image.network(e.avatar!),
-                title: Text('#${e.id}: ${e.name}'),
-                subtitle: e.description == null ? null : Text(e.description!),
-              ),
-            )
-            .toList(),
+      appBar: AppBar(
+        title: const Text('Chats'),
       ),
+      body: ListView(
+        children: chats.values.map(MyChatPreviewListTile.new).toList(),
+      ),
+    );
+  }
+}
+
+class MyChatPreviewListTile extends StatelessWidget {
+  MyChatPreviewListTile(this.e, {Key? key}) : super(key: key ?? ValueKey(e.id));
+
+  final SjChatDto e;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(
+          e.avatar ??
+              'https://raw.githubusercontent.com/julien-gargot/images-placeholder/master/placeholder-square.png',
+        ),
+      ),
+      title: Text('#${e.id}: ${e.name}'),
+      subtitle: e.description == null ? null : Text(e.description!),
+      onTap: () {
+        Navigator.of(context).restorablePushNamed(MyChat.routeName(e.id));
+      },
+    );
+  }
+}
+
+class MyChat extends StatefulWidget {
+  MyChat(this.chatId, {Key? key}) : super(key: key ?? ValueKey(chatId));
+
+  final int chatId;
+
+  static const routeNamePrefix = '/chat/';
+  static String routeName(int chatId) => '$routeNamePrefix$chatId';
+  static int routeNameChatId(String routeName) =>
+      int.parse(routeName.substring(routeNamePrefix.length));
+
+  @override
+  State<MyChat> createState() => _MyChatState();
+}
+
+class _MyChatState extends State<MyChat> {
+  int get chatId => widget.chatId;
+
+  var lastUpdateMsgs = DateTime(2022);
+
+  SjChatDto? chat;
+
+  late final client = SharedAppData.getValue<Type, StudyJamClient>(
+    context,
+    StudyJamClient,
+    () => StudyJamClient(),
+  );
+
+  final msgs = <int, SjMessageDto>{};
+
+  var initial = true;
+  @override
+  void didChangeDependencies() {
+    if (initial) {
+      initial = false;
+      loadData();
+    }
+    super.didChangeDependencies();
+  }
+
+  Future<void> loadData() async {
+    await Future.delayed(const Duration(microseconds: 1));
+    if (!mounted) return;
+    showLoadingDialog(context);
+    try {
+      final sp = await SharedPreferences.getInstance();
+      lastUpdateMsgs = DateTime.parse(
+        sp.getString('lastUpdateMsgs/$chatId') ??
+            DateTime(2022).toIso8601String(),
+      );
+      final msgs = jsonDecode(sp.getString('msgs/$chatId') ?? '{}') as Map?;
+      if (msgs != null) {
+        this.msgs.addAll(msgs.map((key, value) =>
+            MapEntry(int.parse(key), SjMessageDto.fromJson(value))));
+      }
+    } finally {
+      Navigator.pop(context);
+    }
+    await refresh();
+  }
+
+  Future<void> refresh() async {
+    showLoadingDialog(context);
+    try {
+      final sp = await SharedPreferences.getInstance();
+
+      final indexes = await client.getUpdates(
+        msgs: lastUpdateMsgs,
+      );
+      final chats = (await client.getChatsByIds([chatId]));
+      if (chats.isNotEmpty) chat = chats.single;
+
+      final msgsI = indexes.msgs?[chatId];
+      if (msgsI?.isNotEmpty ?? false) {
+        msgsI!;
+        final l = ((msgsI.length - 1) ~/ 1000) + 1;
+        for (var i = 0; i < l; i++) {
+          final msgs = await client.getMessagesByIds(
+            msgsI.skip(i * 1000).take(1000).toList(),
+          );
+          for (final e in msgs) {
+            if (e.updated.isAfter(lastUpdateMsgs)) {
+              lastUpdateMsgs = e.updated;
+            }
+            this.msgs[e.id] = e;
+          }
+        }
+        refreshItemsInListView();
+      }
+
+      await sp.setString(
+          'lastUpdateMsgs/$chatId', lastUpdateMsgs.toIso8601String());
+      final msgs = this.msgs.map(
+            (key, value) => MapEntry(key.toString(), value.toJson()),
+          );
+      await sp.setString('msgs/$chatId', jsonEncode(msgs));
+    } finally {
+      Navigator.pop(context);
+    }
+  }
+
+  int itemsCount = 0;
+  List<SjMessageDto> items = [];
+  Widget itemBuilderFunc(BuildContext context, int index) {
+    return MyMsgListTile(items[index]);
+  }
+
+  void refreshItemsInListView() {
+    itemBuilder =
+        (BuildContext context, int index) => itemBuilderFunc(context, index);
+    itemsCount = msgs.length;
+    items = msgs.values.toList()..sort((a, b) => b.id.compareTo(a.id));
+    setState(() {});
+  }
+
+  late IndexedWidgetBuilder itemBuilder = itemBuilderFunc;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(chat?.name ?? 'Unnamed')),
+      body: ListView.builder(
+        itemBuilder: itemBuilder,
+        itemCount: itemsCount,
+        reverse: true,
+      ),
+    );
+  }
+}
+
+class MyMsgListTile extends StatelessWidget {
+  MyMsgListTile(this.e, {Key? key}) : super(key: key ?? ValueKey(e.id));
+
+  final SjMessageDto e;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text('#${e.id}: ${e.updated}'),
+      subtitle: Text(e.text ?? ''),
     );
   }
 }
